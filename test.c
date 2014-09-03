be9c8e2c333e13d1dee0684cfdb44604575d1e59
kmowery <kmowery@gmail.com>
2014-09-02 17:55:13 -0700

    Add a start at unit tests for double conversion, add, and neg

--- test.c
+++ test.c
@@ -7,6 +7,7 @@
 #include <stddef.h>
 #include <setjmp.h>
 #include <cmocka.h>
+#include <math.h>
 
 #include "ftfp.h"
 
@@ -22,36 +23,85 @@ void p(fixed f) {
   printf("n: %s\n", buf);
 }
 
+
+#define unit_fixint_dbl(name) unit_test(convert_dbl_##name)
+#define CONVERT_DBL(name, d, bits) static void convert_dbl_##name(void **state) { \
+  fixed f = bits; \
+  fixed g = fix_convert_double(d); \
+  assert_memory_equal( &f, &g, sizeof(fixed)); \
+}
+
+CONVERT_DBL(zero, 0, 0x00000);
+CONVERT_DBL(one, 1, 0x20000);
+CONVERT_DBL(one_neg, -1, 0xfffe0000);
+CONVERT_DBL(two, 2, 0x40000);
+CONVERT_DBL(two_neg, -2, 0xfffc0000);
+CONVERT_DBL(many, 1000.4, 0x7d0cccc);
+CONVERT_DBL(many_neg, -1000.4, 0xf82f3334);
+CONVERT_DBL(frac, 0.5342, 0x11180);
+CONVERT_DBL(inf_pos, INFINITY, F_INF_POS);
+CONVERT_DBL(inf_neg, -INFINITY, F_INF_NEG);
+CONVERT_DBL(nan, nan("0"), F_NAN);
+
+#define unit_add(name) unit_test(add_##name)
+#define ADD_CUST(name, op1, op2, result) static void add_##name(void **state) { \
+  fixed o1 = fix_convert_double(op1); \
+  fixed o2 = fix_convert_double(op2); \
+  fixed added = fix_add(o1,o2); \
+  fixed expected = result; \
+  assert_true( FIX_EQ(added, expected) ); \
+}
+#define ADD(name, op1, op2, val) ADD_CUST(name, op1, op2, fix_convert_double(val))
+
+ADD(one_zero,      1,     0,     1);
+ADD(one_one,       1,     1,     2);
+ADD(fifteen_one,   15,    1,     16);
+ADD_CUST(overflow, 1<<13, 1<<13, F_INF_POS);
+// TODO: negative overflow, NaNs, fractions
+
+#define unit_neg(name) unit_test(neg_##name)
+#define NEG_CUST(name, op1, result) static void neg_##name(void **state) { \
+  fixed o1 = fix_convert_double(op1); \
+  fixed negd = fix_neg(o1); \
+  fixed expected = result; \
+  assert_true( FIX_EQ(negd, expected) ); \
+}
+#define NEG(name, op1, val) NEG_CUST(name, op1, fix_convert_double(val))
+
+NEG(zero,    0, 0);
+NEG(one,     1, -1);
+NEG(one_neg, -1, 1);
+NEG_CUST(inf, INFINITY, F_INF_NEG);
+NEG_CUST(inf_neg, -INFINITY, F_INF_POS);
+NEG_CUST(nan, nan("0"), F_NAN);
+
 int main(int argc, char** argv) {
-
-  fixed f = FIXINT(15);
-  fixed g = FIXINT(1);
-  fixed h;
-
-  p(f);
-  p(g);
-
-  f = FIXINT(1 << 13);
-  g = FIXINT(1 << 13);
-  p(f);
-
-  h = fix_add(f,g);
-
-  p(h);
-
-  h = fix_neg(h);
-  p(h);
-
-  f = FIXINT( 1<<12);
-  h = fix_neg(f);
-  p(f);
-  p(h);
-
-  f = FIXINT( (1 << 14) -1 );
-  p(f);
 
   const UnitTest tests[] = {
     unit_test(null_test_success),
+    unit_fixint_dbl(zero),
+    unit_fixint_dbl(one),
+    unit_fixint_dbl(one_neg),
+    unit_fixint_dbl(two),
+    unit_fixint_dbl(two_neg),
+    unit_fixint_dbl(many),
+    unit_fixint_dbl(many_neg),
+    unit_fixint_dbl(frac),
+    unit_fixint_dbl(inf_pos),
+    unit_fixint_dbl(inf_neg),
+    unit_fixint_dbl(nan),
+
+    unit_add(one_zero),
+    unit_add(one_one),
+    unit_add(fifteen_one),
+    unit_add(overflow),
+
+    unit_neg(zero),
+    unit_neg(one),
+    unit_neg(one_neg),
+    unit_neg(inf),
+    unit_neg(inf_neg),
+    unit_neg(nan),
   };
   return run_tests(tests);
 }
