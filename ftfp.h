83e3c0b8ab5f061b4b83090d905a98e57834ee4e
David Kohlbrenner <david.kohlbrenner@gmail.com>
2014-09-11 11:33:15 -0700

    Switched return statements to using bitops instead of ternary statements, ternary statements considered harmful

--- ftfp.h
+++ ftfp.h
@@ -32,6 +32,10 @@ typedef uint32_t fixed;
 #define FIX_IS_INF_POS(f) ((f&FLAGS_MASK) == F_INF_POS)
 #define FIX_IS_INF_NEG(f) ((f&FLAGS_MASK) == F_INF_NEG)
 
+#define FIX_IF_NAN(isnan) (((isnan) | ((isnan) << 1)) & F_NAN)
+#define FIX_IF_INF_POS(isinfpos) (((isinfpos) | ((isinfpos) << 1)) & F_INF_POS)
+#define FIX_IF_INF_NEG(isinfneg) (((isinfneg) | ((isinfneg) << 1)) & F_INF_NEG)
+
 /* Returns true if the numbers are equal (NaNs are always unequal.) */
 /* TODO: will short-circuit. Fix! */
 #define FIX_EQ(op1, op2) (!(FIX_IS_NAN(op1) || FIX_IS_NAN(op2)) || \
