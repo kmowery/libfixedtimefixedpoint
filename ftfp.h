198b2c9c5c29fae492dde4d6996d4429805d90c0
kmowery <kmowery@gmail.com>
2014-10-06 15:09:47 -0700

    Remove short-circuiting from FIX_EQ macros

--- ftfp.h
+++ ftfp.h
@@ -46,16 +46,17 @@ typedef uint32_t fixed;
 #define FIX_IF_INF_NEG(isinfneg) (((isinfneg) | ((isinfneg) << 1)) & F_INF_NEG)
 
 /* Returns true if the numbers are equal (NaNs are always unequal.) */
-/* TODO: will short-circuit. Fix! */
-#define FIX_EQ(op1, op2) (!(FIX_IS_NAN(op1) | FIX_IS_NAN(op2)) && \
-    ((FIX_IS_INF_POS(op1) && FIX_IS_INF_POS(op2)) || \
-    (FIX_IS_INF_NEG(op1) && FIX_IS_INF_NEG(op2)) || \
+#define FIX_EQ(op1, op2) ( \
+    !(FIX_IS_NAN(op1) | FIX_IS_NAN(op2)) & \
+    ((FIX_IS_INF_POS(op1) & FIX_IS_INF_POS(op2)) | \
+    (FIX_IS_INF_NEG(op1) & FIX_IS_INF_NEG(op2)) | \
     (op1 == op2)))
 
 /* Returns true if the numbers are equal (and also if they are both NaN) */
-#define FIX_EQ_NAN(op1, op2) ((FIX_IS_NAN(op1) && FIX_IS_NAN(op2)) || \
-    (FIX_IS_INF_POS(op1) && FIX_IS_INF_POS(op2)) || \
-    (FIX_IS_INF_NEG(op1) && FIX_IS_INF_NEG(op2)) || \
+#define FIX_EQ_NAN(op1, op2) ( \
+    (FIX_IS_NAN(op1) & FIX_IS_NAN(op2)) | \
+    (FIX_IS_INF_POS(op1) & FIX_IS_INF_POS(op2)) | \
+    (FIX_IS_INF_NEG(op1) & FIX_IS_INF_NEG(op2)) | \
     (op1 == op2))
 
 #define FIX_CMP(op1, op2) ({ \
